add898fdaaf51efb1b6ccf39624a02beefd2f056
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-11-11 21:50:50 +0300

    call term exe

--- detector/main.cpp
+++ detector/main.cpp
@@ -116,8 +116,7 @@ void handle_event(int fan_fd) {
                                 eventsCount++;
                                 if (eventsCount == SUS_EVENT_NUMB) {
                                     std::cout << "Suspicious process: " << metadata->pid << "\n";
-                                    exit(EXIT_SUCCESS);
-                                    // terminate process
+                                    terminate_executable(metadata->pid);
                                 }
                             }
 
