8a2594599c954b33c6657a0512ad7f6979557e73
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-12-15 19:42:06 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "database/db.hpp"
 
 int main(int argc, char** argv) {
-    Storage database = new Storage();
+    Storage *database = new Storage();
     database.connect();
     database.initDB("../database/init_db.sql");
 
