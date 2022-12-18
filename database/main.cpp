4b1e294d0f24fe0935692d51ddc27fcbce9dcb79
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-12-18 17:54:03 +0300

    fix

--- database/main.cpp
+++ database/main.cpp
@@ -2,13 +2,14 @@
 #include "file_repo.hpp"
 #include "models/file.hpp"
 #include <iostream>
+#include <unistd.h>
 
 int main(){
     Storage db = Storage();
     db.connect();
     db.initDB("init_db.sql");
     std::vector<char> content{'1', '2', '3'};
-    File file("/home/ok", content, 3);
+    File file("/home/ok", content, 3, ::getpid());
     db.File()->insertFile(file);
     db.close();
 
