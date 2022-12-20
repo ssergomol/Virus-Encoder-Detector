f2ee814f40cf8b2b91ea872dcd7294510807e8f2
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-12-20 22:07:21 +0300

    fix

--- database/file_repo.cpp
+++ database/file_repo.cpp
@@ -90,6 +90,7 @@ bool FileRepo::contains(const std::string& path) const {
     CHECK_F(rc == SQLITE_OK, "Bind failed: %s\n", sqlite3_errmsg(store->getDB()));
 
     rc = sqlite3_step(stmt);
+    sqlite3_reset(stmt);
     sqlite3_finalize(stmt);
 
     // IF no rows returned
--- detector/detector.cpp
+++ detector/detector.cpp
@@ -51,17 +51,15 @@ void Detector::terminate_executable(int pid) {
 }
 
 void Detector::addToDatabase(int pid) {
-//
-//    char exePath[PATH_MAX];
-//    std::string linkToExe;
-//
-//    linkToExe = "/proc/" + std::to_string(pid) + "/exe";
-//    ssize_t len = readlink(linkToExe.c_str(), exePath, sizeof(exePath) - 1);
-//    if (len != -1) {
-//        exePath[len] = '\0';
-//    }
+    char exePath[PATH_MAX];
+    std::string linkToExe;
 
-    std::string filePath = "/home/ssergomol/Repos/Virus-Encoder-Detector/encoder/encoder";
+    linkToExe = "/proc/" + std::to_string(pid) + "/exe";
+    ssize_t len = readlink(linkToExe.c_str(), exePath, sizeof(exePath) - 1);
+    if (len != -1) {
+        exePath[len] = '\0';
+    }
+
 //    std::string filePath = exePath;
 //    if (!DB->File()->contains(exePath)) {
 ////        std::vector<char> content{'1', '2', '3'};
@@ -72,11 +70,10 @@ void Detector::addToDatabase(int pid) {
 //        LOG_F(INFO, "File %s is already in database", exePath);
 //    }
 
-//    std::vector<char> content{'1', '2', '3'};
-//    std::string filePath = exePath;
+    std::string filePath = exePath;
     File file(filePath, pid);
     if (!this->DB->File()->contains(filePath)) {
-        this->DB->File()->insertFile(file);
+//        this->DB->File()->insertFile(file);
     }
     std::cout << filePath << std::endl;
     if (true) {
