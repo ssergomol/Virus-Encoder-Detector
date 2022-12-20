6e9e7564afb8d53b6d6307753ff964fa88b57c34
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-12-20 22:42:55 +0300

    fix

--- encoder/main.cpp
+++ encoder/main.cpp
@@ -3,7 +3,7 @@
 namespace fs = std::filesystem;
 
 int main(int argc, char **argv) {
-//    fs::path file = argv[1];
+    fs::path file = argv[1];
 //    Encoder encoder;
     Encoder::encodeFile(fs::absolute(file));
 //    encoder.start_encoder(argc, argv);
