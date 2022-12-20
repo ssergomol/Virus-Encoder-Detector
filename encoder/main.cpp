40a4bda5e33e6b7d286a22f26c599d08ef7c9b0e
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-12-20 17:44:00 +0300

    test

--- encoder/main.cpp
+++ encoder/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "encoder.hpp"
+namespace fs = std::filesystem;
 
 int main(int argc, char **argv) {
-    fs::path file = argv[1];
+//    fs::path file = argv[1];
     Encoder encoder;
-    encoder.encodeFile(file);
+    encoder.start_encoder(argc, argv)
 }
