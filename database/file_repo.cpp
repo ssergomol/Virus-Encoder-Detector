47ed4525772aafce00149f2be9b08346b90c2e3a
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-12-20 22:16:25 +0300

    fix

--- database/file_repo.cpp
+++ database/file_repo.cpp
@@ -90,7 +90,7 @@ bool FileRepo::contains(const std::string& path) const {
     CHECK_F(rc == SQLITE_OK, "Bind failed: %s\n", sqlite3_errmsg(store->getDB()));
 
     rc = sqlite3_step(stmt);
-    if (rc != SQLITE3_OK) {
+    if (rc != SQLITE_OK) {
         LOG_F(FATAL, "sqlite3_step is blocked");
     }
     sqlite3_finalize(stmt);
